b784581b801f6628a8b0876d8263bdd63d0b084b
newracket <newracket@users.noreply.github.com>
2024-04-11 15:43:45 -0700

    Add support for PATCH and DELETE methods in Discord event API

--- src/util/discord.ts
+++ src/util/discord.ts
@@ -18,7 +18,12 @@ export const imageUrlToBase64 = async (url: string) => {
   return `data:image/${extension};base64,${Buffer.from(blob).toString('base64')}`
 };
 
-export const pingDiscordWebhook = (message: string, url: string, title: string, location: string) => {
+export const pingDiscordWebhook = (message: string, url?: string, title?: string, location?: string) => {
+  let description = "";
+  if (url || title || location) {
+    description = `**Event title**: ${title}\n**Location**: ${location}\n**Link**: ${url}`;
+  }
+  
   const hook = new WebhookClient({
     url: process.env.DISCORD_WEBHOOK_URL as string,
   });
@@ -27,7 +32,7 @@ export const pingDiscordWebhook = (message: string, url: string, title: string, 
     embeds: [
       {
         title: message,
-        description: `**Event title**: ${title}\n**Location**: ${location}\n**Link**: ${url}`,
+        description,
       },
     ],
   });
@@ -57,6 +62,43 @@ export const createDiscordEvent = async (
       privacy_level: 2,
     },
   });
+
+  return response;
+};
+
+export const editDiscordEvent = async (
+  eventID: string,
+  title?: string,
+  start?: string,
+  end?: string,
+  location?: string,
+  description?: string,
+  image?: string
+) => {
+  const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_BOT_TOKEN as string);
+
+  const response = await rest.patch(`/guilds/${process.env.ACM_GUILD_ID}/scheduled-events/${eventID}`, {
+    body: {
+      name: title,
+      scheduled_start_time: start,
+      scheduled_end_time: end,
+      description,
+      image,
+      entity_metadata: {
+        location,
+      },
+      entity_type: 3,
+      privacy_level: 2,
+    },
+  });
+
+  return response;
+};
+
+export const deleteDiscordEvent = async (eventID: string) => {
+  const rest = new REST({ version: "10" }).setToken(process.env.DISCORD_BOT_TOKEN as string);
+
+  const response = await rest.delete(`/guilds/${process.env.ACM_GUILD_ID}/scheduled-events/${eventID}`);
 
   return response;
 };
