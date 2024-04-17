56271f94e4e4f4cdcff56ad75b1d13212cacbc35
Aniket Gupta <newracket@users.noreply.github.com>
2024-04-17 16:40:16 -0700

    Add support for PATCH and DELETE methods in Discord event API (#7)

--- pages/api/v1/discord/event.ts
+++ pages/api/v1/discord/event.ts
@@ -6,7 +6,7 @@ import NextCors from "nextjs-cors";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   await NextCors(req, res, {
-    methods: ["POST"],
+    methods: ["POST", "PATCH", "DELETE"],
     origin: "*",
     optionsSuccessStatus: 200,
   });
@@ -17,8 +17,22 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     return res.status(400).json({ error: err });
   }
 
-  // Validate body provided
+  // Ensure body exists
   if (!req.body) return res.status(400).json({ error: "No body provided" });
+
+  switch (req.method) {
+    case "POST":
+      return post_handler(req, res);
+    case "PATCH":
+      return patch_handler(req, res);
+    case "DELETE":
+      return delete_handler(req, res);
+    default:
+      return res.status(405).json({ error: "Method not allowed" });
+  }
+}
+
+async function post_handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   const { title, start, end, description, location, image } = req.body;
 
   // Validate body values exist and are acceptable
@@ -62,7 +76,48 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
       title, location
     );
 
-    return res.status(200).json({ message: "Event created successfully!" });
+    return res.status(200).json({ message: `Event created with ID: ${eventID}` });
+  } catch (err) {
+    return res.status(400).json({ error: JSON.stringify(err) });
+  }
+}
+
+async function patch_handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  if (!req.body) return res.status(400).json({ error: "No body provided" });
+  const { eventID, title, start, end, description, location, image } = req.body;
+
+  // Validate body values exist and are acceptable
+  if (!eventID) return res.status(400).json({ error: "Missing required body field: eventID" });
+  if (start && new Date(start) < new Date())
+    return res.status(400).json({ error: "Start date cannot be in the past" });
+  if (start && new Date(end) < new Date())
+    return res.status(400).json({ error: "End date cannot be in the past" });
+
+  try {
+    await discord.editDiscordEvent(eventID, title, start, end, description, location, image);
+    discord.pingDiscordWebhook(
+      ":bangbang: Discord event modified!",
+      `${process.env.ACM_DISCORD_INVITE_URL}?event=${eventID}`,
+      title, location
+    );
+
+    return res.status(200).json({ message: `Event modified with ID: ${eventID}` });
+  } catch (err) {
+    return res.status(400).json({ error: JSON.stringify(err) });
+  }
+}
+
+async function delete_handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const { eventID } = req.body;
+
+  // Validate body values exist and are acceptable
+  if (!eventID) return res.status(400).json({ error: "Missing required body field: eventID" });
+
+  try {
+    await discord.deleteDiscordEvent(eventID);
+    discord.pingDiscordWebhook(":bangbang: Discord event deleted!");
+
+    return res.status(200).json({ message: `Event deleted with ID: ${eventID}` });
   } catch (err) {
     return res.status(400).json({ error: JSON.stringify(err) });
   }
