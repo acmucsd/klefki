3a4a9fc97eabab84cdd390032d7510091b33fc41
newracket <newracket@users.noreply.github.com>
2024-08-16 17:39:42 -0700

    added API endpoints for patch/delete for calendar events

--- pages/api/v1/calendar/event.ts
+++ pages/api/v1/calendar/event.ts
@@ -3,13 +3,25 @@ import { calendar } from "@/util";
 import type { Data } from "@/util/types";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  // Validate POST request type
-  if (req.method !== "POST") return res.status(400).json({ error: "Invalid request method" });
-
   // Validate body provided
   if (!req.body) return res.status(400).json({ error: "No body provided" });
-  const { name, start, end, description, location, eventLink } = req.body;
+  
+  // Validate request type
+  switch (req.method) {
+    case "POST":
+      return post_handler(req, res);
+    case "PATCH":
+      return patch_handler(req, res);
+    case "DELETE":
+      return delete_handler(req, res);
+    default:
+      return res.status(405).json({ error: "Invalid request method" });
+  }
+}
 
+async function post_handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const { name, start, end, description, location, eventLink } = req.body;
+  
   // Validate body values exist and are acceptable
   if (!name) return res.status(400).json({ error: "Missing required body field: name" });
   if (!start) return res.status(400).json({ error: "Missing required body field: start" });
@@ -25,10 +37,49 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
 
   // Create Google calendar event
   try {
-    await calendar.createCalendarEvent(name, start, end, location, description, eventLink);
-    return res.status(200).json({ message: "Event created successfully!" });
+    const eventID = await calendar.createCalendarEvent(name, start, end, location, description, eventLink);
+    return res.status(200).json({ message: `Event created with calendar ID: ${eventID}` });
   } catch (err) {
-    console.error(err);
+    return res.status(400).json({ error: JSON.stringify(err) });
+  }
+}
+
+async function patch_handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const { eventID, name, start, end, description, location, eventLink } = req.body;
+  
+  // Validate body values exist and are acceptable
+  if (!eventID) return res.status(400).json({ error: "Missing required body field: eventID" });
+  if (!name) return res.status(400).json({ error: "Missing required body field: name" });
+  if (!start) return res.status(400).json({ error: "Missing required body field: start" });
+  if (!end) return res.status(400).json({ error: "Missing required body field: end" });
+  if (!description)
+    return res.status(400).json({ error: "Missing required body field: description" });
+  if (!location) return res.status(400).json({ error: "Missing required body field: location" });
+  if (!eventLink) return res.status(400).json({ error: "Missing required body field: eventLink" });
+  if (new Date(start) < new Date())
+    return res.status(400).json({ error: "Start date cannot be in the past" });
+  if (new Date(end) < new Date())
+    return res.status(400).json({ error: "End date cannot be in the past" });
+
+  try {
+    await calendar.editCalendarEvent(eventID, name, start, end, location, description, eventLink);
+    return res.status(200).json({ message: `Event modified with ID: ${eventID}` });
+  } catch (err) {
+    return res.status(400).json({ error: JSON.stringify(err) });
+  }
+}
+
+async function delete_handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  const { eventID } = req.body;
+
+  // Validate body values exist and are acceptable
+  if (!eventID) return res.status(400).json({ error: "Missing required body field: eventID" });
+
+  try {
+    await calendar.deleteCalendarEvent(eventID);
+
+    return res.status(200).json({ message: `Event deleted with ID: ${eventID}` });
+  } catch (err) {
     return res.status(400).json({ error: JSON.stringify(err) });
   }
 }
--- src/util/calendar.ts
+++ src/util/calendar.ts
@@ -19,26 +19,67 @@ export async function createCalendarEvent(
   endTime: string,
   location: string,
   description: string,
-  eventLink: string
+  eventLink: string,
+): Promise<string | null | undefined> {
+  const calendar = await initializeCalendar();
+  const eventDescription = `${description}\n\nLocation: ${location}\nEvent Link: ${eventLink}`;
+  
+  const response = await calendar.events.insert({
+    calendarId: process.env.GOOGLE_CALENDAR_ID,
+    requestBody: {
+      summary: title,
+      description: eventDescription,
+      location,
+      start: {
+        dateTime: startTime,
+      },
+      end: {
+        dateTime: endTime,
+      },
+    },
+  });
+  
+  return response?.data?.id;
+}
+
+export async function editCalendarEvent(
+  eventId: string,
+  title: string,
+  startTime: string,
+  endTime: string,
+  location: string,
+  description: string,
+  eventLink: string,
 ): Promise<void> {
   const calendar = await initializeCalendar();
   const eventDescription = `${description}\n\nLocation: ${location}\nEvent Link: ${eventLink}`;
-  try {
-    const response = await calendar.events.insert({
-      calendarId: process.env.GOOGLE_CALENDAR_ID,
-      requestBody: {
-        summary: title,
-        description: eventDescription,
-        location,
-        start: {
-          dateTime: startTime,
-        },
-        end: {
-          dateTime: endTime,
-        },
+  
+  await calendar.events.update({
+    calendarId: process.env.GOOGLE_CALENDAR_ID,
+    eventId,
+    requestBody: {
+      summary: title,
+      description: eventDescription,
+      location,
+      start: {
+        dateTime: startTime,
       },
-    });
-  } catch (err) {
-    console.error(err);
-  }
+      end: {
+        dateTime: endTime,
+      },
+    },
+  });
+}
+
+
+
+export async function deleteCalendarEvent(
+  eventId: string,
+): Promise<void> {
+  const calendar = await initializeCalendar();
+  
+  await calendar.events.delete({
+    calendarId: process.env.GOOGLE_CALENDAR_ID,
+    eventId,
+  });
 }
