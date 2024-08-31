488a6ff974555b0722ac68b56dfa21f101375480
newracket <newracket@users.noreply.github.com>
2024-08-30 22:35:03 -0700

    minor changes

--- pages/api/v1/calendar/event.ts
+++ pages/api/v1/calendar/event.ts
@@ -9,31 +9,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
   // Validate request type
   switch (req.method) {
     case "POST":
-      return post_handler(req, res);
+      return createCalendarEvent(req, res);
     case "PATCH":
-      return patch_handler(req, res);
+      return editCalendarEvent(req, res);
     case "DELETE":
-      return delete_handler(req, res);
+      return deleteCalendarEvent(req, res);
     default:
       return res.status(405).json({ error: "Invalid request method" });
   }
 }
 
-async function post_handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+async function createCalendarEvent(req: NextApiRequest, res: NextApiResponse<Data>) {
   const { name, start, end, description, location, eventLink } = req.body;
-  
-  // Validate body values exist and are acceptable
-  if (!name) return res.status(400).json({ error: "Missing required body field: name" });
-  if (!start) return res.status(400).json({ error: "Missing required body field: start" });
-  if (!end) return res.status(400).json({ error: "Missing required body field: end" });
-  if (!description)
-    return res.status(400).json({ error: "Missing required body field: description" });
-  if (!location) return res.status(400).json({ error: "Missing required body field: location" });
-  if (!eventLink) return res.status(400).json({ error: "Missing required body field: eventLink" });
-  if (new Date(start) < new Date())
-    return res.status(400).json({ error: "Start date cannot be in the past" });
-  if (new Date(end) < new Date())
-    return res.status(400).json({ error: "End date cannot be in the past" });
+  const validationError = validateBody(res, name, start, end, description, location, eventLink);
+  if (validationError) return res.status(400).json({ error: validationError });
 
   // Create Google calendar event
   try {
@@ -44,23 +33,13 @@ async function post_handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   }
 }
 
-async function patch_handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+async function editCalendarEvent(req: NextApiRequest, res: NextApiResponse<Data>) {
   const { eventID, name, start, end, description, location, eventLink } = req.body;
-  
-  // Validate body values exist and are acceptable
+  const validationError = validateBody(res, name, start, end, description, location, eventLink);
+  if (validationError) return res.status(400).json({ error: validationError });
   if (!eventID) return res.status(400).json({ error: "Missing required body field: eventID" });
-  if (!name) return res.status(400).json({ error: "Missing required body field: name" });
-  if (!start) return res.status(400).json({ error: "Missing required body field: start" });
-  if (!end) return res.status(400).json({ error: "Missing required body field: end" });
-  if (!description)
-    return res.status(400).json({ error: "Missing required body field: description" });
-  if (!location) return res.status(400).json({ error: "Missing required body field: location" });
-  if (!eventLink) return res.status(400).json({ error: "Missing required body field: eventLink" });
-  if (new Date(start) < new Date())
-    return res.status(400).json({ error: "Start date cannot be in the past" });
-  if (new Date(end) < new Date())
-    return res.status(400).json({ error: "End date cannot be in the past" });
 
+  // Edit Google calendar event
   try {
     await calendar.editCalendarEvent(eventID, name, start, end, location, description, eventLink);
     return res.status(200).json({ message: `Event modified with ID: ${eventID}` });
@@ -69,12 +48,13 @@ async function patch_handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   }
 }
 
-async function delete_handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+async function deleteCalendarEvent(req: NextApiRequest, res: NextApiResponse<Data>) {
   const { eventID } = req.body;
 
   // Validate body values exist and are acceptable
   if (!eventID) return res.status(400).json({ error: "Missing required body field: eventID" });
 
+  // Delete Google calendar event
   try {
     await calendar.deleteCalendarEvent(eventID);
 
@@ -82,4 +62,21 @@ async function delete_handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   } catch (err) {
     return res.status(400).json({ error: JSON.stringify(err) });
   }
+}
+
+function validateBody(res: NextApiResponse<Data>, name: string, start: string, end: string, description: string, location: string, eventLink: string): string | null {
+  // Validate body values exist and are acceptable
+  if (!name) return "Missing required body field: name";
+  if (!start) return "Missing required body field: start";
+  if (!end) return "Missing required body field: end";
+  if (!description)
+    return "Missing required body field: description";
+  if (!location) return "Missing required body field: location";
+  if (!eventLink) return "Missing required body field: eventLink";
+  if (new Date(start) < new Date())
+    return "Start date cannot be in the past";
+  if (new Date(end) < new Date())
+    return "End date cannot be in the past";
+
+  return null;
 }
