178facb7977dfd539298101f292ef8f0d92118e7
= <aniket.9upt@gmail.com>
2024-12-02 19:31:07 -0800

    work

--- next.config.js
+++ next.config.js
@@ -18,7 +18,15 @@ const nextConfig = {
         ]
       }
     ]
-  }
+  },
+  webpack: (config, options) => {
+    config.module.rules.push({
+      test: /\.node/,
+      use: 'node-loader'
+    })
+ 
+    return config
+  },
 };
 
 module.exports = nextConfig;
